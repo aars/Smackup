9b65c5ef49f4d78af4d2b23150c1b0f4e3743305
Aaron Adrianus Henricus Heesakkers <aaronh@xs4all.nl>
2009-09-22 17:23:08 +0200

    Starting to get the hang of it.

--- backup.py
+++ backup.py
@@ -1,5 +1,7 @@
 # Import python modules
-import sys, ConfigParser, logging, logging.handlers, pprint
+import sys
+import ConfigParser
+import pprint
 
 # Import local modules
 import lib.RSyncBackup
@@ -11,17 +13,20 @@ config = {
     "dest"   : "etc/destinations.conf"
 }
 
-for k, v in config.iteritems():
+for k, v in config.items():
     try:
         config[k] = ConfigParser.ConfigParser()
-        config[k].read(v)
+        config[k].readfp(open(v))
+        config[k]._file = v
     except IOError as (errno, strerror):
-        print "Failed to read configfile: " + strerror
-        sys.exit()
+        sys.exit("Failed to read configuration file '{0}': {1}".format(v, strerror))
     except:
-        print sys.exc_info()
-        sys.exit()
+        type, value = sys.exc_info()[:2]
+        sys.exit("Unknown error of type {0} occured: {1}".format(type, value))
 
-print config['system'].sections()
+# Check logging configuration
+if not config['system'].has_section('Log'):
+    sys.exit("Missing section 'Log' in file {0}".format(config['system']._file))
+if not config['system'].has_option('Log', 'fie'):
+    sys.exit("Missing option 'file' from section 'Log' in file {0}".format(config['system']._file))
 
-print "Hello aids"
