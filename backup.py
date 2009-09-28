6542b9468c749fc126e02514210762849e3c5ed3
Aaron Adrianus Henricus Heesakkers <aaronh@xs4all.nl>
2009-09-28 19:29:10 +0200

    Use of new objects.

--- backup.py
+++ backup.py
@@ -1,9 +1,22 @@
+#!/usr/bin/env python
 # Import python modules
 import sys
 import pprint
 
 # Import local modules
-import lib.RSyncBackup
 import lib.Configuration
+import lib.Backup
 
-Config = lib.Configuration.Configuration()
+try:
+    Config = lib.Configuration.Configuration()
+except lib.Configuration.ConfigException, e:
+    print "Configuration error: ", sys.exc_info()[1]
+except Exception, e:
+    print "Unexpected error: ", sys.exc_info()[1]
+finally:
+    sys.exit('Exiting')
+
+Backup = lib.Backup.Backup(Config)
+
+if Backup.time_to_run():
+        Backup.run()
