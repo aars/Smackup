144475b88ed4d7379254b1920d29f89619177bd3
Aaron Adrianus Henricus Heesakkers <aaronh@xs4all.nl>
2009-09-28 19:27:06 +0200

    Added error checking

--- lib/Configuration.py
+++ lib/Configuration.py
@@ -1,16 +1,29 @@
 import sys
 import ConfigParser
 
+class ConfigException (Exception):
+    pass
+
+class MissingDestinationException (ConfigException):
+    def __init__(self, section):
+        ConfigException.__init__(self, 
+            "No destination defined in files section '{0}'".format(section))
+
+class UndefinedDestinationException (ConfigException):
+    def __init__(self, section, destination):
+        ConfigException.__init__(self, 
+            "Files section '{0}' refers to undefined destination '{1}'".format(section, destination))
+
 class Configuration:
 
     required    = {
         'system' : {
-            'Log' : ['file', 'lrf']
+            'Log' : ['file', 'lockfile', 'lastrunfile'],
+            'Run' : ['interval']
         },
-        'dest'   : {}
     }
 
-    files = { 
+    configfiles = { 
         'system' : "etc/system.conf", 
         'files'  : "etc/files.conf",
         'dest'   : "etc/destinations.conf"
@@ -20,7 +33,8 @@ class Configuration:
 
     def __init__(self):
         self.load()
-        self.check()
+        self.check_required()
+        self.check_destinations()
 
     def __setattr__(self, k, v):
         self.config[k] = v
@@ -29,24 +43,33 @@ class Configuration:
         return self.config[k]
 
     def load(self):
-        for k, v in self.files.items():
+        for k, v in self.configfiles.items():
             try:
                 self.config[k] = ConfigParser.ConfigParser()
                 self.config[k].readfp(open(v))
                 self.config[k]._file = v
             except IOError as (errno, strerror):
-                sys.exit("Failed to read configuration file '{0}': {1}".format(v, strerror))
+                raise ConfigException(
+                    "Failed to read configuration file '{0}': {1}".format(v, strerror))
             except:
-                type, value = sys.exc_info()[:2]
-                sys.exit("Unknown error of type {0} occured: {1}".format(type, value))
+                raise
 
-    def check(self):
+    def check_required(self):
         for name, dict in self.required.items():
-            if len(dict) < 1: continue
             for section in dict:
-                if not len(self.required[name][section]): continue
                 if not self.config[name].has_section(section):
                     sys.exit("Missing section '{0}' ({1})".format(name, self.config[name]._file))
                 for option in self.required[name][section]:
                     if not self.config[name].has_option(section, option):
                         sys.exit("Missing option '{0}' in section '{1}' ({2})".format(option, name, self.config[name]._file))
+
+    def check_destinations(self):
+        for section in self.config['files'].sections():
+            if not self.config['files'].get(section, 'dest'):
+                raise MissingDestinationException(section)
+
+            destinations = self.config['files'].get(section, 'dest')
+            for destination in [str.strip() for str in destinations.split(',')]:
+                if not self.config['dest'].has_section(destination):
+                    raise UndefinedDestinationException(section, destination)
+        
